f4ceedf3e594a6d2ace72d18b4ce0ae3521a3c8d
Catur Andi Pamungkas <catur.andi.pamungkas@gmail.com>
2019-03-25 01:22:47 +0700

    Add New Column

--- database/migrations/2019_03_24_120243_create_todos_table.php
+++ database/migrations/2019_03_24_120243_create_todos_table.php
@@ -16,6 +16,7 @@ class CreateTodosTable extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
